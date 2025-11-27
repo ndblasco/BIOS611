e8f2c3554765e6bc1d6919d970fdc2d30d3a7469
ndblasco <nblasco@unc.edu>
2025-11-27 16:17:32 -0500

    Fantasy points >0

--- scripts/data_preprocessing.R
+++ scripts/data_preprocessing.R
@@ -2,10 +2,17 @@ library(tidyverse)
 
 df <- read_csv("data/df.csv")
 
-final <- df |> 
-  mutate(Team = ifelse(Team == "LA", "LAR", Team))|>
-  filter(!Team %in% c("FA")) %>%
-  filter(year != 2025) %>%
+analysis_df <- df |> 
+  filter(!Pos %in% c("LB", "DL", "DB")) |>
+  dplyr::select(-TacklesTot, -TacklesAst, -TacklesSck, -TacklesTfl,
+                -TurnoverInt, -TurnoverFrcFum, -TurnoverFumRec,
+                -ScoreIntTd, -ScoreFumTd, -ScoreBlkTd, -ScoreSaf,
+                -ScoreDef2ptRet, -Blk, -PDef, -QBHit,
+                -ReturnIntYds, -ReturnFumYds, -`FanPtsAgainst-pts`)|>
+  mutate(Team = ifelse(Team %in% c("LA", "LAR"), "LAR", Team))|>
+  filter(!Team %in% c("FA")) |>
+  filter(year != 2025) |>
+  filter(TotalPoints > 0) |>
   mutate(across(where(is.numeric), ~replace_na(., 0)))|>
   mutate(
     `FgMiss_0-19`   = replace_na(`FgMiss_0-19`, 0),
@@ -13,8 +20,7 @@ final <- df |>
     `FgMiss_30-39`  = replace_na(`FgMiss_30-39`, 0)
   )
 
-final$year <- as.integer(final$year)
+analysis_df$year <- as.integer(analysis_df$year)
 
-
-write_csv(final, "data/analysis_df.csv")
+write_csv(analysis_df, "data/analysis_df.csv")
 analysisdf <- read_csv("data/analysis_df.csv")
